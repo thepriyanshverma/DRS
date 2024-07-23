665b44cb408c2d1283db75e24cc9684def9d4e8f
thepriyanshverma <priyanshvermaofficial@gmail.com>
2024-07-23 17:58:33 +0000

    Comment

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
   return "ABW1"
 
+
 if __name__ == '__main__':
   print("Inside the if")
-  app.run(host='0.0.0.0',debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)
+
+#DRV
